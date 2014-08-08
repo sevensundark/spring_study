9bc08374c3d717159632e7300250d5a14446e7ac
sevensundark <zhanglei_seven@hotmail.com>
2014-08-08 16:34:39 +0800

    add spring chapter6

---
+++ springmvc-chapter6/src/controller/paramtype/CookieValueTypeController.java
@@ -0,0 +1,31 @@
+package controller.paramtype;
+
+import javax.servlet.http.Cookie;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/method/param/annotation")
+public class CookieValueTypeController {
+
+	@RequestMapping(value = "/cookie")
+	public String test(
+			@CookieValue(value = "JSESSIONID", defaultValue = "") String sessionId) {
+
+		System.out.println(sessionId);
+
+		return "success";
+	}
+
+	@RequestMapping(value = "/cookie2")
+	public String test2(
+			@CookieValue(value = "JSESSIONID", defaultValue = "") Cookie sessionId) {
+
+		System.out.println(sessionId.getName());
+
+		return "success";
+	}
+
+}
---
+++ springmvc-chapter6/src/controller/paramtype/HeaderValueTypeController.java
@@ -0,0 +1,23 @@
+package controller.paramtype;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/method/param/annotation")
+public class HeaderValueTypeController {
+
+	@RequestMapping(value = "/header")
+	public String test(@RequestHeader("User-Agent") String userAgent,
+			@RequestHeader(value = "Accept") String[] accepts) {
+
+		System.out.println(userAgent);
+		System.out.println(Arrays.toString(accepts));
+
+		return "success";
+	}
+
+}
